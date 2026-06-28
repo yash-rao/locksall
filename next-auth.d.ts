b8f0db0a8c4e8d59050cc21758c96abf6c8787dc
Yash Barot <69378969+yash-rao@users.noreply.github.com>
2026-06-28 15:42:14 -0400

    Type auth role fields

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,19 +1,24 @@
 import type { DefaultSession } from "next-auth";
 
+type LocksAllRole = "USER" | "ADMIN";
+
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
+      role?: LocksAllRole;
     } & DefaultSession["user"];
   }
 
   interface User {
     id: string;
+    role?: LocksAllRole;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     id?: string;
+    role?: LocksAllRole;
   }
 }
